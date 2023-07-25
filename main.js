0ff325875c26bfba9ee80f4e187c3105231f5d10
Anuj-Patel123 <106400113+Anuj-Patel123@users.noreply.github.com>
2023-07-25 15:42:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function setup() {
+    canvas = createCanvas(550, 550);
+    canvas.position(1000, 150);
+    video = createCapture(VIDEO);
+    video.size(550, 550);
+    video.position(194, 150)
+}
